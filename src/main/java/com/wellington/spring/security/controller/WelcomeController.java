cb9924233874fc10422ba6106eace38e30668fbb
Wellington Vasconcelos <wltvasc@hotmail.com>
2022-08-18 15:18:22 -0300

    Configure adapter | Autenticacao com Banco de Dados

--- src/main/java/com/wellington/spring/security/controller/WelcomeController.java
+++ src/main/java/com/wellington/spring/security/controller/WelcomeController.java
@@ -1,6 +1,5 @@
-package com.wellington.spring.security;
+package com.wellington.spring.security.controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
@@ -10,12 +9,10 @@ public class WelcomeController {
         return "Welcome to My Spring Boot Web API";
     }
     @GetMapping("/users")
-    @PreAuthorize("hasAnyRole('MANAGERS', 'USERS')")
     public String users() {
         return "Authorized user";
     }
     @GetMapping("/managers")
-   @PreAuthorize("hasAnyRole('MANAGERS')")
     public String managers() {
         return "Authorized manager";
     }
